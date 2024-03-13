005ba0b2ac264e7aa61c6dbe48562fd327a4a374
dim4 <dmitrvls@gmail.com>
2024-03-13 23:04:34 +0600

    Вывод списка имен из бд

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,7 +10,8 @@ void main() async {
           apiKey: "AIzaSyC6q8fLmuYyo38HaLiNDjcB7KshbS1D0rc",
           appId: "1:226975447509:web:24ef0da7a7ca85c684255c",
           messagingSenderId: "226975447509",
-          projectId: "dmsg-1d1c5")
+          projectId: "dmsg-1d1c5",
+          databaseURL:"https://dmsg-1d1c5-default-rtdb.europe-west1.firebasedatabase.app/")
   );
 
   runApp(const MyApp());
@@ -23,21 +26,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Chats - DMSG',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -48,16 +36,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -65,27 +43,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-  final Future<FirebaseApp> _fApp = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final users = RxList<String>();
+    DatabaseReference _testRef = FirebaseDatabase.instance.reference().child('users');
+
+    _testRef.onValue.listen((event) {
+      final data = event.snapshot.value;
+      final userList = (data as Map<String, dynamic>).values.cast<String>().toList(); // Преобразование данных в список имен
+      users.value = userList; // Обновляем RxList пользователей
+
+      // Для примера выводим список имен пользователей
+    });
     return Scaffold(
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
@@ -96,28 +65,18 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body:
-          FutureBuilder(
-            future: _fApp,
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return Text("Error");
-              } else if (snapshot.hasData) {
-                return Text("Firebase initialized");
-              } else {
-                return CircularProgressIndicator();
-              }
-            },
-          )
+        body: Obx(() => Column(
+          children: users.map((user) => Text(user)).toList(),
+        ))
       // Row(
       //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
       //   children: <Widget>[
       //     Expanded(
       //       child: ListView.builder(
-      //         itemCount: 20,
+      //         itemCount: users.length,
       //         itemBuilder: (context, index) {
       //           return ListTile(
-      //             title: Text('Chat ${index + 1}'),
+      //             title: Text(users[index]),
       //           );
       //         },
       //       ),
@@ -125,6 +84,18 @@ class _MyHomePageState extends State<MyHomePage> {
       //     const FlutterLogo(),
       //   ],
       // ),
+      // FutureBuilder(
+      //   future: _fApp,
+      //   builder: (context, snapshot) {
+      //     if (snapshot.hasError) {
+      //       return Text("Error");
+      //     } else if (snapshot.hasData) {
+      //       return Text("Firebase initialized");
+      //     } else {
+      //       return CircularProgressIndicator();
+      //     }
+      //   },
+      // )
     );
   }
 }
